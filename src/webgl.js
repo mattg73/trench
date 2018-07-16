b44fdfd4e1c8fe7e1122ec772ab8ac0fbcde827c
Matt Greenhalgh <mattg73@users.noreply.github.com>
2018-07-16 14:21:43 +0100

    added webgl-stats

--- src/webgl.js
+++ src/webgl.js
@@ -1,5 +1,6 @@
 import { Clock } from 'three';
 import {Stats} from 'three-stats';
+import RendererStats from 'three-webgl-stats';
 import Debug from './debug';
 import StateMachine from './stateMachine';
 
@@ -10,6 +11,12 @@ class WebGL {
     this.stats = new Stats();
     this.stats.showPanel(0);
     document.body.appendChild(this.stats.dom);
+
+    const rendererStats = new RendererStats();
+    rendererStats.domElement.style.position	= 'absolute'
+    rendererStats.domElement.style.left	= '0px'
+    rendererStats.domElement.style.bottom	= '0px'
+    document.body.appendChild( rendererStats.domElement )
 
     Debug.init();
 
